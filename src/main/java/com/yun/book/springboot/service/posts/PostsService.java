c3637e3339f0a726802b98f0d7124a5e01c616e7
Yunok <ahyz0569@gmail.com>
2021-02-04 00:40:20 +0900

    등록 API

---
+++ src/main/java/com/yun/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.yun.book.springboot.service.posts;
+
+import com.yun.book.springboot.domain.posts.PostsRepository;
+import com.yun.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
